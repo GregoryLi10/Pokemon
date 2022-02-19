55ef22505ffabd3fd1dbe326f79209779e346950
GregoryLi10 <90701941+GregoryLi10@users.noreply.github.com>
2022-02-19 18:22:58 -0500

    .

--- Pokemon/src/pokemon/Melatonin.java
+++ Pokemon/src/pokemon/Melatonin.java
@@ -3,7 +3,7 @@ package pokemon;
 public class Melatonin extends Pokemon{
 
 	public Melatonin() {
-		super("Melatonin", 0, 376, "Melatonin.png", new Move[] {new Move("Math.random() damage", 0, 102, 0), new Move("Math.random() damage", 0, 101, 0), new Move("slep", 0, 15, 1), new Move("slep", 0, 15, 1)});
+		super("Melatonin", 0, 401, "Melatonin.png", new Move[] {new Move("Math.random() damage", 0, 77, 0), new Move("Math.random() damage", 0, 76, 0), new Move("slep", 0, 15, 1), new Move("slep", 0, 15, 1)});
 	}
 
 }
